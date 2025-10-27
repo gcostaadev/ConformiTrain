945e15be353013a75eca070597ca06b146c1f18d
Gabriel Costa Dias <gabriecostadias8@gmail.com>
2025-10-27 14:57:08 -0300

    feat: Integra testes BDD com Docker Compose ao pipeline de CI

--- Program.cs
+++ Program.cs
@@ -37,27 +37,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        // Tenta obter o DbContext
-        var context = services.GetRequiredService<ConformiTrain.Persistence.ConformiTrainDbContext>();
-
-        // Aplica quaisquer migrations pendentes.
-        // Cria a base de dados se ela não existir.
-        context.Database.Migrate();
-
-        Console.WriteLine("----> Migrations aplicadas com sucesso."); // Log para confirmação
-    }
-    catch (Exception ex)
-    {
-        // Regista qualquer erro durante a migração
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "----> Ocorreu um erro durante a aplicação das migrations.");
-        // Considerar parar a aplicação se as migrations falharem: throw;
-    }
-}
-
 app.Run();
